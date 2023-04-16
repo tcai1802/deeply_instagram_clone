8eebd91493e1ffeb7311aaa029f45c7d95b6cfcd
ai.trancong <ai.trancong@papagroup.net>
2023-04-16 13:09:04 +0700

    * upload media

--- backend/middleware/uploadMedia.js
+++ backend/middleware/uploadMedia.js
@@ -2,11 +2,20 @@ const cloudinary = require('cloudinary').v2;
 
 const handleUploadMedia = async (fileList) => {
     const url_list= []
-    for(index in fileList) {
-        await cloudinary.uploader.upload(fileList[index].path, {upload_preset: "ml_default",}, (error, result)=>{
-            url_list.push(result.url)
-        });
-    }  
+    try {
+        for(index in fileList) {
+            await cloudinary.uploader.upload(fileList[index].path, {upload_preset: "ml_default",}, (error, result)=>{
+                url_list.push(result.url)
+            });
+        }  
+    } catch (error) {
+        res.status(500).json({
+            "code": "failed",
+            "message": err,
+            "data": null
+        })
+    }
+
     return url_list
 }
 
